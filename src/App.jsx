185fb369db7229427cfee2b23963a7146f989fa6
Akshay-V2 <184760060+Akshay-V2@users.noreply.github.com>
2024-12-16 10:07:02 +0530

    rough implementation of mode switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import { useState } from 'react';
 import '../src/App.css';
 
 import TimeWidget from './components/TimeWidget';
 import Switcher from './components/switcher';
 
 function App() {
+    const [currentMode, setCurrentMode] = useState(0);
+
+    const handleModeChange = (newMode) => {
+        setCurrentMode(newMode);
+        console.log(newMode);
+    }
+
     return (
         <>
         <div className='bg-frame'>
-            <TimeWidget/>
-            <Switcher/>
+            {currentMode === 0  ? <TimeWidget/> : '' }
+            <Switcher onModeChange={handleModeChange}/>
         </div>
         </>
     )
--- src/components/Switcher.jsx
+++ src/components/Switcher.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import { useState } from 'react';
 import { CaretRight, CaretLeft } from '@phosphor-icons/react';
 
-const Switcher = () => {
+const Switcher = (props) => {
+    const [mode, setMode] = useState(0);
+
+    const handleSwitch = (direction) => {
+        let newMode;
+        if (direction == 'fwd' && mode <= 2) {
+            newMode = mode + 1;
+            setMode(newMode);
+        } else if (direction == 'rwd' && mode >= 1) {
+            newMode = mode - 1;
+            setMode(newMode);
+        }
+
+        if (props.onModeChange) {
+            props.onModeChange(newMode);
+        }
+    }
+
     return (
         <>
         <div className='switcher-icons-holder'>
-            <CaretLeft size={24} weight="fill" className='switcher-icons'/>
+            <CaretLeft size={24} weight="fill" className='switcher-icons' onClick={() => handleSwitch('rwd')}/>
             <div className='switcher-dots-holder'>
-                <div className='switcher-dots-active'></div>
-                <div className='switcher-dots-inactive'></div>
-                <div className='switcher-dots-inactive'></div>
-                <div className='switcher-dots-inactive'></div>
+                <div className={mode === 0 ? 'switcher-dots-active' : 'switcher-dots-inactive'}></div>
+                <div className={mode === 1 ? 'switcher-dots-active' : 'switcher-dots-inactive'}></div>
+                <div className={mode === 2 ? 'switcher-dots-active' : 'switcher-dots-inactive'}></div>
+                <div className={mode === 3 ? 'switcher-dots-active' : 'switcher-dots-inactive'}></div>
             </div>
-            <CaretRight size={24} weight="fill" className='switcher-icons'/>
+            <CaretRight size={24} weight="fill" className='switcher-icons' onClick={() => handleSwitch('fwd')}/>
         </div>
         </>
     )
